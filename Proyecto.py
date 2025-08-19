e1aa7b35485bb9d4529d1beaf0d109bc283a503b
Jose Sulecio <josesulecio6@gmail.com>
2025-08-19 15:23:13 -0600

    Se crea la clase Actividad, se deja ya con su constructor

--- Proyecto.py
+++ Proyecto.py
@@ -1,2 +1,7 @@
-
-#Hola
+#Se crea la clase Actividad
+class Actividad:
+    def __init__(self, titulo, fecha, categoria, prioridad):
+        self.titulo = titulo
+        self.fecha = fecha
+        self.categoria = categoria
+        self.prioridad = prioridad
