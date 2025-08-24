ce778a4b4a0626d3d14ece094dcd0cb7cb4a1247
Dennys <dennys028ss@gmail.com>
2025-08-24 15:17:49 -0600

    Filtros por Prioridad

--- Proyecto.py
+++ Proyecto.py
@@ -10,6 +10,7 @@ class Actividad:
 class GestorActividades:
     def __init__(self):
         self.actividades = []
+        self.categorias_validas = []
 
     def agregar_actividad(self):
         print("\n--- Agregar nueva actividad ---")
@@ -40,3 +41,57 @@ class GestorActividades:
         nueva = Actividad(titulo, fecha, categoria, prioridad)
         self.actividades.append(nueva)
         print("Actividad agregada con éxito.")
+
+
+    #filtros por prioridad o tipo de eventos
+    def filtrar_por_prioridad(self, prioridad_buscar):
+        if prioridad_buscar < 1 or prioridad_buscar > 5:
+            print("⚠️ La prioridad debe estar entre 1 y 5.")
+            return []
+
+        actividades_filtradas = []
+        for act in self.actividades:
+            if act.prioridad == prioridad_buscar:
+                actividades_filtradas.append(act)
+        return actividades_filtradas
+
+    def filtrar_por_categoria(self, categoria_buscar):
+        categoria_buscar = categoria_buscar.lower()
+
+        if categoria_buscar not in self.categorias_validas:
+            print(f"⚠️ Categoria no valida. Use una de: {', '.join(self.categorias_validas)}")
+            return []
+
+        actividades_filtradas = []
+        for act in self.actividades:
+            if act.categoria == categoria_buscar:
+                actividades_filtradas.append(act)
+        return actividades_filtradas
+
+    def filtrar_por_prioridad_y_categoria(self, prioridad_buscar, categoria_buscar):
+        if prioridad_buscar < 1 or prioridad_buscar > 5:
+            print("⚠️ La prioridad debe estar entre 1 y 5.")
+            return []
+
+        categoria_buscar = categoria_buscar.lower()
+
+        if categoria_buscar not in self.categorias_validas:
+            print(f"⚠️ Categoría no valida. Use una de: {', '.join(self.categorias_validas)}")
+            return []
+
+        actividades_filtradas = []
+        for act in self.actividades:
+            if act.prioridad == prioridad_buscar and act.categoria == categoria_buscar:
+                actividades_filtradas.append(act)
+        return actividades_filtradas
+
+    def mostrar_actividades(self, actividades, mensaje=""):
+        if not actividades:
+            print("No hay actividades que mostrar.")
+            return
+
+        if mensaje:
+            print(f"\n{mensaje}")
+
+        for i, actividad in enumerate(actividades, 1):
+            print(f"{i}. {actividad.titulo} - {actividad.fecha} - {actividad.categoria} - Prioridad: {actividad.prioridad}")
