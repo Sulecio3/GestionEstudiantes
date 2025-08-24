10aa9b3e08a8a696313cb966700eb8e01302be55
Ferry <lopezdelcidferrydejesus@gmail.com>
2025-08-24 17:22:51 -0600

    Agregue dos funciones, convertir fecha a numero para posteriormente en otra ordenar por fecha usando tuplas y recorriendolas por numero

--- Proyecto.py
+++ Proyecto.py
@@ -1,6 +1,6 @@
 #Se crea la clase Actividad
 class Actividad:
-    def __init__(self, titulo, fecha, categoria, prioridad):
+    def __init__(self, titulo, fecha, categoria, prioridad, descripcion):
         self.titulo = titulo
         self.fecha = fecha
         self.categoria = categoria
@@ -34,6 +34,29 @@ class GestorActividades:
                 return fecha_show
             except ValueError:
                 print('ERROR. Por favor ingresar numeros validos.')
+
+    def convertir_fecha_a_numero(self, fecha_str):
+        #Aca convertimos la fecha a un numero normal, para ordenarlo de menor a mayor
+        partes = fecha_str.split('/')
+        dia = int(partes[0])
+        mes = int(partes[1])
+        year = int(partes[2])
+        numero_fecha = year * 10000 + mes * 100 + dia
+        return numero_fecha
+
+    def ordenar_por_fecha(self, actividades):
+        # Creamos una lista de tuplas (número_fecha, actividad)
+        actividades_con_numeros = []
+        for actividad in actividades:
+            numero_fecha = self.convertir_fecha_a_numero(actividad.fecha)
+            actividades_con_numeros.append((numero_fecha, actividad))
+        # Ordenar por el número de fecha con la funcion sort
+        actividades_con_numeros.sort()
+        # Extraer solo las actividades ordenadas
+        actividades_ordenadas = []
+        for numero, actividad in actividades_con_numeros:
+            actividades_ordenadas.append(actividad)
+        return actividades_ordenadas
     def agregar_actividad(self):
         print("\n--- Agregar nueva actividad ---")
         # Se piden datos
@@ -50,3 +73,10 @@ class GestorActividades:
         nueva = Actividad(titulo, fecha, categoria, prioridad)
         self.actividades.append(nueva)
         print("Actividad agregada con éxito.")
+
+    def listar_actividades(self):
+        print("\n ----- ACTIVIDADES -----")
+        if not self.actividades:
+            print("No hay actividades registradas.")
+            return
+
